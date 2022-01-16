067939e5345db0830d243e88ca453527305b8c4b
whes1015 <44525760+whes1015@users.noreply.github.com>
2022-01-16 21:10:37 +0800

    Add files via upload

--- src/main/java/event/eventlistener.java
+++ src/main/java/event/eventlistener.java
@@ -4,17 +4,22 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import core.logger;
 import core.network;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
+import java.text.MessageFormat;
+import java.util.Calendar;
 import java.util.Objects;
 
 import static main.main.BlockArray;
@@ -23,9 +28,25 @@ import static main.whes1015.DATA;
 public class eventlistener implements Listener {
 
     @EventHandler
+    public void PlayerJoinEvent(PlayerJoinEvent event){
+        Player player = event.getPlayer();
+        JsonElement Data= JsonParser.parseString(DATA.toString());
+        JsonObject data=Data.getAsJsonObject();
+        data.addProperty("Type","PlayerJoinEvent");
+        data.addProperty("FormatVersion", 1);
+        data.addProperty("Player",player.getName());
+        data.addProperty("Uuid", String.valueOf(player.getUniqueId()));
+        data.addProperty("Ip", String.valueOf(player.getAddress()));
+        data.addProperty("Op",player.isOp());
+        JsonObject jsonObject=network.Post(data);
+        if (jsonObject.get("response").getAsJsonObject().get("response").getAsString().equals("Unable to confirm")){
+            event.getPlayer().sendMessage(ChatColor.RED+"\u7121\u6cd5\u78ba\u8a8d\u4f60\u7684\u5e33\u865f\u6578\u64da\uff0c\u6578\u64da\u6709\u907a\u5931\u98a8\u96aa\uff0c\u5efa\u8b70\u76e1\u5feb\u7d81\u5b9a\u5e33\u865f");
+        }
+    }
+
+    @EventHandler
     public void BlockBreakEvent(BlockBreakEvent event) {
         if(event.isCancelled()) return;
-        Bukkit.broadcastMessage(event.getBlock().getBlockData().getAsString());
         if(event.getPlayer().getGameMode()!= GameMode.SURVIVAL) return;
         JsonArray Array =BlockArray.getAsJsonArray();
         int Find=-1;
@@ -79,6 +100,7 @@ public class eventlistener implements Listener {
         JsonElement Data= JsonParser.parseString(DATA.toString());
         JsonObject data=Data.getAsJsonObject();
         data.addProperty("Type","PlayerQuitEvent");
+        data.addProperty("FormatVersion", 1);
         data.addProperty("Uuid", String.valueOf(player.getUniqueId()));
         network.Post(data);
     }
@@ -89,6 +111,7 @@ public class eventlistener implements Listener {
         JsonElement Data= JsonParser.parseString(DATA.toString());
         JsonObject data=Data.getAsJsonObject();
         data.addProperty("Type","PlayerRespawnEvent");
+        data.addProperty("FormatVersion", 1);
         data.addProperty("Uuid", String.valueOf(player.getUniqueId()));
         network.Post(data);
     }
