2a946693e34a519c6494042d5fda5fa00e6bd354
whes1015 <44525760+whes1015@users.noreply.github.com>
2022-01-30 23:14:16 +0800

    Add files via upload

--- src/main/java/event/eventlistener.java
+++ src/main/java/event/eventlistener.java
@@ -2,15 +2,14 @@ package event;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import core.logger;
 import org.bukkit.Material;
 import org.bukkit.Statistic;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerStatisticIncrementEvent;
-
-import java.util.Objects;
 
 import static function.dataupdate.DataUpdate;
 import static main.main.*;
@@ -18,32 +17,37 @@ import static main.main.*;
 public class eventlistener implements Listener {
 
     @EventHandler
+    public void PlayerJoinEvent(PlayerJoinEvent event){
+        event.getPlayer().sendMessage("\u524d\u5f80 https://exptechweb.mywire.org/ \u53ef\u4ee5\u67e5\u8a62\u904a\u6232\u7d71\u8a08\u6578\u64da");
+    }
+
+    @EventHandler
     public void PlayerStatisticIncrementEvent(PlayerStatisticIncrementEvent event) {
-            DataUpdate(event.getPlayer(),main(event.getPlayer()));
+        DataUpdate(event.getPlayer(), main(event.getPlayer()));
     }
 
     public static JsonArray main(Player player) {
         JsonArray jsonArray = new JsonArray();
         for (int i = 0; i < statisticComplex.size(); i++) {
-            String Name=statisticComplex.get(i).getAsJsonObject().get("name").getAsString();
+            String Name = statisticComplex.get(i).getAsJsonObject().get("name").getAsString();
             JsonObject JsonObject = new JsonObject();
             JsonArray JsonArray = new JsonArray();
             for (int x = 0; x < Json.size(); x++) {
                 JsonObject jsonObject = new JsonObject();
                 String name = Json.get(x).getAsJsonObject().get("name").getAsString();
-                String type=Json.get(x).getAsJsonObject().get("type").getAsString();
-                if(Objects.equals(type, "block")&&!Objects.equals(Name, "ENTITY_KILLED_BY")&&!Objects.equals(Name, "KILL_ENTITY")) {
-                    if (player.getStatistic(Statistic.valueOf(Name), Material.valueOf(name)) != 0) {
-                        jsonObject.addProperty("name", name);
-                        jsonObject.addProperty("value", player.getStatistic(Statistic.valueOf(Name), Material.valueOf(name)));
-                        JsonArray.add(jsonObject);
+                String type=Json.get(x).getAsJsonObject().get("type").getAsJsonArray().toString();
+               try {
+                    if (type.contains(Name)) {
+                        if (player.getStatistic(Statistic.valueOf(Name), Material.valueOf(name)) != 0) {
+                            jsonObject.addProperty("name", name);
+                            jsonObject.addProperty("value", player.getStatistic(Statistic.valueOf(Name), Material.valueOf(name)));
+                            JsonArray.add(jsonObject);
+                        }
                     }
-                }else if(Objects.equals(type, "entity")){
-                    if (player.getStatistic(Statistic.valueOf(Name), EntityType.valueOf(name)) != 0) {
-                        jsonObject.addProperty("name", name);
-                        jsonObject.addProperty("value", player.getStatistic(Statistic.valueOf(Name), EntityType.valueOf(name)));
-                        JsonArray.add(jsonObject);
-                    }
+                } catch (IllegalArgumentException e) {
+                    logger.log("TRACE","DataRecord_eventlistener" , "Name: "+Name+" name: "+name);
+                    logger.log("ERROR", "DataRecord_eventlistener", e.getMessage());
+                    return null;
                 }
             }
             JsonObject.addProperty("name", Name);
